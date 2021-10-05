8bdfa90e880b7e0827a541f3aee78177f8d836d9
spl33t <spl33t@ya.ru>
2021-10-05 22:34:42 +0300

    readme upd

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <main class="page-body">
   <div class="content">
 
-    <?php while (have_posts()) : the_post() ?>
 
-    <?php endwhile ?>
+    <?php if (have_posts()) : ?>
+      <?php while (have_posts()) : the_post(); ?>
+        <a href="<?php echo get_post_permalink(); ?>">
+          <h2><?php the_title(); ?></h2>
+        </a>
+        <article>
+          <?php the_content(); ?>
+        </article>
+        <br />
+      <?php endwhile; ?>
+    <?php endif; ?>
+
 
   </div>
 </main>
