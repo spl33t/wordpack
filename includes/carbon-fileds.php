645f48775a22a8e9e0e52101b6d45b34ad4f73bb
spl33t <spl33t@ya.ru>
2022-03-04 17:37:14 +0300

    crb fix for child theme

--- includes/carbon-fileds.php
+++ includes/carbon-fileds.php
@@ -7,6 +7,6 @@
 
 add_action('after_setup_theme', 'crb_load');
 function crb_load() {
-    require_once(get_stylesheet_directory() . '/vendor/autoload.php');
+    require_once(get_template_directory() . '/vendor/autoload.php');
     \Carbon_Fields\Carbon_Fields::boot();
 }
